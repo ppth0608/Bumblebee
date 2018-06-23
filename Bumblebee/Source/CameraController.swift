e0329d74c7aa5c8246e63a8fd5361e8e3e165477
taehyun.park <taehyun.park@navercorp.com>
2018-06-23 13:54:43 +0900

    프로젝트 세팅

---
+++ Bumblebee/Source/CameraController.swift
@@ -0,0 +1,9 @@
+//
+//  CameraController.swift
+//  Bumblebee
+//
+//  Created by Naver on 2018. 6. 23..
+//  Copyright © 2018년 benpark. All rights reserved.
+//
+
+import Foundation
---
+++ Bumblebee/Source/CameraViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CameraViewController.swift
+//  Bumblebee
+//
+//  Created by Naver on 2018. 6. 23..
+//  Copyright © 2018년 benpark. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
